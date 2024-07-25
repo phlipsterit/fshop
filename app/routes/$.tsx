c7f36f4c296d9f6ed1821f4a2007a28f72635e1d
phlipsterit <phlipsterit@outlook.com>
2024-07-25 10:57:11 +0200

    add debug info

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,28 +1,30 @@
-import { json } from "@remix-run/node";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 import {
   getStoryblokApi,
   useStoryblokState,
   StoryblokComponent,
+  ISbStoriesParams,
 } from "@storyblok/react";
 
 export default function Page() {
-  let { story } = useLoaderData();
+  let { story, debugInfo } = useLoaderData<typeof loader>();
   story = useStoryblokState(story);
 
   return (
     <>
       <StoryblokComponent blok={story.content} />
+      <div>{JSON.stringify(debugInfo, null, 2)}</div>
     </>
   );
 }
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   let slug = params["*"] ?? "home";
   let blogSlug = params["*"] === "blog/" ? "blog/home" : null;
 
-  let sbParams = {
+  let sbParams: ISbStoriesParams = {
     version: "draft",
   };
 
@@ -37,5 +39,10 @@ export const loader = async ({ params }) => {
     throw new Response("Not Found", { status: 404 });
   }
 
-  return json({ story: data?.story });
+  return json({
+    story: data?.story,
+    debugInfo: {
+      requestUrl: request.url,
+    },
+  });
 };
