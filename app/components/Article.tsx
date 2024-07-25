817d6ac954864d32ce8452ff55f3ddedeaccae41
phlipsterit <phlipsterit@outlook.com>
2024-07-25 15:18:57 +0200

    support for focal points

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -4,6 +4,12 @@ import type { ArticlePageStoryblok } from "~/component-types-sb";
 export const Article = ({ blok }: { blok: ArticlePageStoryblok }) => (
   <main {...storyblokEditable(blok)} key={blok._uid} className="px-4">
     <h1>{blok.Name}</h1>
-    {blok.Bilde && <img src={blok.Bilde.filename}></img>}
+    {blok.Bilde && (
+      <img
+        src={`${blok.Bilde.filename}/m/400x400${
+          blok.Bilde.focus ? "/filters:focal(" + blok.Bilde.focus + ")" : ""
+        }`}
+      ></img>
+    )}
   </main>
 );
