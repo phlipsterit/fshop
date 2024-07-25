7261caea72500a4bf01c7e78662652a666843322
phlipsterit <phlipsterit@outlook.com>
2024-07-25 15:28:58 +0200

    image ratio depending on screen size

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -5,11 +5,20 @@ export const Article = ({ blok }: { blok: ArticlePageStoryblok }) => (
   <main {...storyblokEditable(blok)} key={blok._uid} className="px-4">
     <h1>{blok.Name}</h1>
     {blok.Bilde && (
-      <img
-        src={`${blok.Bilde.filename}/m/400x400${
-          blok.Bilde.focus ? "/filters:focal(" + blok.Bilde.focus + ")" : ""
-        }`}
-      ></img>
+      <>
+        <img
+          className="md:hidden"
+          src={`${blok.Bilde.filename}/m/400x400${
+            blok.Bilde.focus ? "/filters:focal(" + blok.Bilde.focus + ")" : ""
+          }`}
+        ></img>
+        <img
+          className="max-md:hidden"
+          src={`${blok.Bilde.filename}/m/800x400${
+            blok.Bilde.focus ? "/filters:focal(" + blok.Bilde.focus + ")" : ""
+          }`}
+        ></img>
+      </>
     )}
   </main>
 );
